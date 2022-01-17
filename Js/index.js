9c85f50c64e6518592523d0d7941cb72dc5e5c5f
MarioIgnacioLeon <82422017+MarioIgnacioLeon@users.noreply.github.com>
2022-01-17 10:19:44 -0300

    Add scrollreveal
    Scrollreveal

---
+++ Js/index.js
@@ -0,0 +1,6 @@
+/*const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classlist.toggle(".nav-menu_visible");
+});*/
